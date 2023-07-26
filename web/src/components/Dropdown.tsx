a991e9f4d828353019ceeec2e96b04e76d2851d7
nanthony <nanthony@uwaterloo.ca>
2023-07-26 19:50:46 -0400

    Dropdown styling

--- web/src/components/Dropdown.tsx
+++ web/src/components/Dropdown.tsx
@@ -14,8 +14,17 @@ interface DropdownProps {
 
 const Icon = () => {
   return (
-    <svg height="20" width="20" viewBox="0 0 20 20">
-      <path d="M4.516 7.548c0.436-0.446 1.043-0.481 1.576 0l3.908 3.747 3.908-3.747c0.533-0.481 1.141-0.446 1.574 0 0.436 0.445 0.408 1.197 0 1.615-0.406 0.418-4.695 4.502-4.695 4.502-0.217 0.223-0.502 0.335-0.787 0.335s-0.57-0.112-0.789-0.335c0 0-4.287-4.084-4.695-4.502s-0.436-1.17 0-1.615z"></path>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="10"
+      height="7"
+      viewBox="0 0 10 7"
+      fill="none"
+    >
+      <path
+        d="M5.744 6.17159C5.34666 6.61401 4.65334 6.61401 4.256 6.17159L0.436019 1.91818C-0.142142 1.27442 0.314743 0.249999 1.18002 0.249999L8.81999 0.25C9.68526 0.25 10.1421 1.27442 9.56398 1.91818L5.744 6.17159Z"
+        fill="#050417"
+      />
     </svg>
   );
 };
@@ -53,7 +62,13 @@ function Dropdown(props: DropdownProps) {
     <div className="test">
       <div className="dropdown-container">
         <div onClick={handleMenuClick} className="dropdown-input">
-          <div className="dropdown-selected-value">{getDisplay()}</div>
+          <div
+            className={`dropdown-selected-${
+              data?.collection ? "value" : placeholder
+            }`}
+          >
+            {getDisplay()}
+          </div>
           <div className="dropdown-tools">
             <div className="dropdown-tool">
               <Icon />
